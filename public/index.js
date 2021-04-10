d199b8c99569483fe044ed9c5eab7e33a25c1953
Your Name <you@example.com>
2021-04-10 20:24:49 +0200

    participaant response done

---
+++ public/index.js
@@ -0,0 +1,9 @@
+function showMessage(){
+  var person = prompt("Please enter your name", "Harry Potter");
+  if (success==="TRUE" ){
+    var person = prompt("Please enter your name", "Harry Potter");
+  }else{
+    var person = prompt("Plesdadasdase enter your name", "Harry Potter");
+  }
+
+}
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -25,43 +24,91 @@ connection.once("open",()=> {
 
 // music schema
 const musicsSchema = new mongoose.Schema({
-  name: String
+  name: String,
+  date: Date
+
 });
 const Music = mongoose.model("Music", musicsSchema);
+
+// participant schema
+const participantSchema = new mongoose.Schema({
+  name: String,
+  present: String,
+  email: String,
+  follower: String,
+  notes: String,
+  updated: { type: Date, default: Date.now }
+
+});
+const Participant = mongoose.model("Participant", participantSchema);
 // #################
 
-
-
 app.get("/", function(req, res) {
-
-  res.render("index", {})
+  res.render("index", {visibility:"hidden",height:"0", text:"" });
 
 });
 app.post("/participant", function(req, res) {
-  console.log(req.body);
+
   const participant = req.body.ParticipantName;
   const bethere = req.body.there;
   const emailAddress = req.body.ParticipantEmail;
   const followers = req.body.ParticipantFollowers;
   const note = req.body.ParticipantNote;
+  const currentDate= new Date;
+  const newParticipant = new Participant ({
+    name: participant,
+    present: bethere,
+    email: emailAddress,
+    follower: followers,
+    notes: note,
+    updated: currentDate
+  });
+  
+  Participant.findOne({ name: participant }, function(err, found){
+      if (err){
+        console.log("MONGOd Error");
+      }else
+      if (found){
+        console.log("found");
+        Participant.deleteOne({ name: participant }, function(err, found){
+          if (err){
+            console.log("deletion error");
+          }else{
+            newParticipant.save(function(err,doc){
+              if(err){
+                console.log("Saving error");
+              }else{
+              res.render("index", { visibility:"visible",height:"50px", text:"Visszajelzésed frissítettük"});
+              }
+          });}
 
-
-
-  res.redirect("/");
+        })
+      }else{
+        console.log("new member");
+        newParticipant.save(function(err,doc){
+          if(err){console.log("Saving error");}
+        });
+        res.render("index", { visibility:"visible",height:"50px", text:"Köszönjük, hogy visszajeleztél."});
+      }
+  });
 })
+
+
 app.post("/music", function(req, res) {
 
   const musicrequest = req.body.music;
+  const currentDate= new Date;
   if (musicrequest===""){
     console.log("request is empty");
   }else{
     const newMusic = new Music({
-      name: musicrequest
+      name: musicrequest,
+      date: currentDate
     });
     newMusic.save();
   }
+  res.render("index", { visibility:"visible",height:"50px", text:"Kérésédet továbbítjuk DJ-nk felé." });
 
-  res.redirect("/");
 })
 app.get("/*", function(req, res) {
   res.redirect("/");
